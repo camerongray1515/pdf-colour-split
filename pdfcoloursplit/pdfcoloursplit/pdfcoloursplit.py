7ab6acc0933eb9654c394800e78d40083fec244b
Cameron Gray <cameron@camerongray.me>
2016-04-04 18:20:08 +0100

    Moved so that pdftoppm is now called directly with a lower resolution reducing the need for tempoary files on disk

--- pdfcoloursplit/pdfcoloursplit/pdfcoloursplit.py
+++ pdfcoloursplit/pdfcoloursplit/pdfcoloursplit.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
-import binascii
+import subprocess
+from binascii import hexlify
 
-def is_ppm_colour(filename):
-    """
-        Takes in filename of PPM file and returns if it contains any colour
-        pixels or not.  Designed for PPM files produced by "pdftoppm" and
-        therefore does not handle comments in the PPM files.
-    """
-    with open(filename, "rb") as f:
-        magic_number = f.readline().decode("UTF-8").split()[0]
-        width, height = f.readline().decode("UTF-8").split()
-        maxval = int(f.readline().decode("UTF-8").split()[0])
+def is_page_colour(pdf_filename, page_number, dpi=10):
+    p = subprocess.Popen([
+        "/usr/bin/pdftoppm",
+        "-r", str(dpi),
+        "-f", str(page_number),
+        "-l", str(page_number),
+        pdf_filename
+    ], stdout=subprocess.PIPE)
 
-        num_bytes = 1 if maxval < 256 else 2
-        # num_bytes=2
+    ppm, stderr = p.communicate()
 
-        bs = f.read(num_bytes*3)
-        while bs:
-            # Takes list of num_bytes*3 bytes and converts it into a list of
-            # three components (one for each of RGB) each containing num_bytes
-            # bytes
-            if num_bytes == 1:
-                colour_values = bs
-            else:
-                colour_values = [bs[x:x+num_bytes] for x in range(0,
-                    num_bytes*3, num_bytes)]
+    ppm = ppm.split(b"\n")
+    magic_number = ppm[0].decode("UTF-8").split()[0]
+    # TODO: Enforce that magic number is P6
+    # width, height = ppm[1].decode("UTF-8").split()
+    maxval = int(ppm[2].decode("UTF-8").split()[0])
+    pixels = b''.join(ppm[3:])
 
-            r,g,b = colour_values
-            if not(r==g and g==b):
-                return True # We encountered a colour pixel so page is colour
+    num_bytes = 1 if maxval < 256 else 2
+    slice_size = num_bytes*3
 
-            bs = f.read(num_bytes*3)
+    for i in range(0, len(pixels), slice_size):
+        slice_pixels = pixels[i:i+slice_size]
+        if num_bytes == 1:
+            colour_values = slice_pixels
+        else:
+            colour_values = [slice_pixels[x:x+num_bytes] for x in range(0,
+                slice_size, num_bytes)]
 
-        return False # No colour pixels were encoutered
+        r,g,b = colour_values
+        if not(r==g and g==b):
+            return True # We encountered a colour pixel so page is colour
+
+    return False # No colour pixels were encoutered
 
 def main():
-    for i in range(0, 60):
-        num = str(i+1).zfill(2)
-        colour = is_ppm_colour("/tmp/pdf/report-{}.ppm".format(num))
-        print("Page {} is{}colour".format(num, " " if colour else " not "))
+    for i in range(1, 61):
+        colour = is_page_colour("/tmp/pdf/final_report.pdf", i)
+        print("Page {} is{}colour".format(i, " " if colour else " not "))
 
 if __name__ == "__main__":
     main()
