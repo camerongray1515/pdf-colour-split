ec55402debe25c56fd28e38d0dca405d6f95686c
Cameron Gray <cameron@camerongray.me>
2016-04-10 17:52:56 +0100

    Simple Celery example working

--- pdfcoloursplit_web/pdfcoloursplit_web/worker.py
+++ pdfcoloursplit_web/pdfcoloursplit_web/worker.py
@@ -1,4 +1,12 @@
 from .config import config
+from celery import Celery
 
-def main():
-    print(config["Celery"]["broker"])
+celery_broker = config["Celery"]["broker"]
+celery_backend = config["Celery"]["backend"]
+app = Celery("worker", broker=celery_broker, backend=celery_backend)
+
+@app.task
+def add(x, y):
+    import time
+    time.sleep(5)
+    return x+y
